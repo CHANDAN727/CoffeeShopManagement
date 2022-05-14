a57cc15772cc95b843fe8714dff221aca698085c
CHANDAN727 <62067152+CHANDAN727@users.noreply.github.com>
2022-05-14 17:43:57 +0530

    Add files via upload

---
+++ StockManager.java
@@ -0,0 +1,166 @@
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+public class StockManager extends javax.swing.JFrame 
+{
+    Connection con;
+    ResultSet rs;
+    PreparedStatement pst;
+    String mid;
+    public StockManager() 
+    {
+        initComponents();
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        b1 = new javax.swing.JButton();
+        l2 = new javax.swing.JLabel();
+        l3 = new javax.swing.JLabel();
+        l4 = new javax.swing.JLabel();
+        b2 = new javax.swing.JButton();
+        t1 = new javax.swing.JTextField();
+        p1 = new javax.swing.JPasswordField();
+        l1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+
+        b1.setText("HOME");
+        b1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                b1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(b1);
+        b1.setBounds(830, 30, 100, 35);
+
+        l2.setForeground(new java.awt.Color(255, 255, 255));
+        l2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        l2.setText("MANGAER ID");
+        l2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        getContentPane().add(l2);
+        l2.setBounds(180, 170, 150, 35);
+
+        l3.setForeground(new java.awt.Color(240, 240, 240));
+        l3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        l3.setText("PASSWORD");
+        l3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        getContentPane().add(l3);
+        l3.setBounds(180, 270, 150, 35);
+
+        l4.setForeground(new java.awt.Color(255, 255, 255));
+        l4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        l4.setText("ENTER MANAGER DETAILS:");
+        getContentPane().add(l4);
+        l4.setBounds(170, 70, 300, 35);
+
+        b2.setText("LOGIN");
+        b2.setVerifyInputWhenFocusTarget(false);
+        b2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                b2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(b2);
+        b2.setBounds(620, 400, 100, 35);
+
+        t1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        getContentPane().add(t1);
+        t1.setBounds(450, 170, 300, 35);
+
+        p1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        getContentPane().add(p1);
+        p1.setBounds(450, 270, 300, 35);
+
+        l1.setIcon(new javax.swing.ImageIcon("C:\\Users\\chand\\Downloads\\mngr.jpg")); // NOI18N
+        getContentPane().add(l1);
+        l1.setBounds(0, 0, 1000, 600);
+
+        setSize(new java.awt.Dimension(1000, 600));
+        setLocationRelativeTo(null);
+    }// </editor-fold>                        
+    private void b1ActionPerformed(java.awt.event.ActionEvent evt) {                                   
+        Home h=new Home();
+        h.setVisible(true);
+        this.hide();
+    }                                  
+
+    @SuppressWarnings("deprecation")
+	private void b2ActionPerformed(java.awt.event.ActionEvent evt) {                                   
+        String name=t1.getText();
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/COFFEE_SHOP?useSSL=false","root","");
+            pst=con.prepareStatement("select * from stk_mgr;");
+            rs=pst.executeQuery();
+            while(rs.next())
+            {
+                if(name.equals(rs.getString(2)))
+                {
+                    mid=rs.getString(1);
+                }
+            }
+            pst=con.prepareStatement("select * from stk_mgr where mgr_id=? and pwd=?");
+            pst.setString(1,t1.getText());
+            pst.setString(2,p1.getText());
+            rs=pst.executeQuery();
+         
+                if(rs.next())
+                {
+                    rs.close();
+                    pst.close();
+                    JOptionPane.showMessageDialog(null,"Login successful Welcome Manager");
+                    System.out.println(name);
+                    new ManagerLogin(name).setVisible(true);
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog(null,"Incorrect credentials");
+                }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        finally
+        {
+            try
+            {
+                rs.close();
+                pst.close();
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+        }        
+    }                                  
+
+    public static void main(String args[]) 
+    {
+       
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new StockManager().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton b1;
+    private javax.swing.JButton b2;
+    private javax.swing.JLabel l1;
+    private javax.swing.JLabel l2;
+    private javax.swing.JLabel l3;
+    private javax.swing.JLabel l4;
+    private javax.swing.JPasswordField p1;
+    private javax.swing.JTextField t1;
+    // End of variables declaration                   
+}
